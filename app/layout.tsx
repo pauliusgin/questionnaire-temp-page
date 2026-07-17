d474ccc8daaa72be27bed4edd7c401ba445717f0
Paulius Giniūnas <paulius.giniunas@gmail.com>
2026-07-18 00:00:50 +0300

    update icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,9 @@ export const metadata: Metadata = {
   manifest: "/site.webmanifest",
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
     ],
     apple: "/apple-touch-icon.png",
   },
